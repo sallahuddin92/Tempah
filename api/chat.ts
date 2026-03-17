f2122faf2bf8916e5cb1de8552c05343388e4d5b
sallahuddin92 <179673687+sallahuddin92@users.noreply.github.com>
2026-03-17 10:47:10 +0800

    Update chat.ts

--- api/chat.ts
+++ api/chat.ts
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     });
 
     const response = await ai.models.generateContent({
-      model: "gemini-1.5-flash",
+      model: "gemini-2.0-flash",
       contents: message,
       config: {
         systemInstruction: `
